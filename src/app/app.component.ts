38c578869756577b749d92f464340ab4eec0213b
Yo <yoelnacho@gmail.com>
2016-06-20 16:55:15 -0300

    Reestructura de archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,20 @@ import { Routes, ROUTER_DIRECTIVES} from '@angular/router';
 // importar componente
 import { LandingPageComponent } from './components/landing-page.component';
 import { PeliculasListComponent } from './components/peliculas-list.component';
-import { CommonFooterComponent } from './components/common-footer.component';
-import { CommonNavComponent } from './components/common-nav.component';
+import { FooterComponent } from './components/commons/footer.component';
+import { NavComponent } from './components/commons/nav.component';
 
 @Component({
   moduleId: module.id,
-  selector: 'curso-angular2-app',
-  templateUrl: 'view/curso-angular2.component.html',
-  styleUrls: ['styles/curso-angular2.component.css'],
+  selector: 'my-app',
+  templateUrl: 'view/app.component.html',
   // en directives indico que componente voy a utilizar
   directives: [
     ROUTER_DIRECTIVES, 
     LandingPageComponent, 
     PeliculasListComponent, 
-    CommonFooterComponent, 
-    CommonNavComponent
+    FooterComponent, 
+    NavComponent
   ]
 })
 
@@ -27,5 +26,6 @@ import { CommonNavComponent } from './components/common-nav.component';
     {path: "/peliculas", component: PeliculasListComponent}
 ]) 
 
-export class CursoAngular2AppComponent {
+export class AppComponent {
+  public title:string;
 }
--- src/app/components/commons/footer.component.ts
+++ src/app/components/commons/footer.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'common-footer',
-  templateUrl: '../view/common/common-footer.component.html'
+  templateUrl: '../../view/commons/footer.component.html'
 })
-export class CommonFooterComponent {
+export class FooterComponent {
   public text:string = "Copyright 2016";
 }
