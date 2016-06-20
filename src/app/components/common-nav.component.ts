fc4bf4871a4703d69515ee0c7068a783b642f42b
Yo <yoelnacho@gmail.com>
2016-06-20 05:10:07 -0300

    componente nav

--- src/app/components/common-nav.component.ts
+++ src/app/components/common-nav.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Routes, ROUTER_DIRECTIVES} from '@angular/router';
  
 @Component({
   moduleId: module.id,
   selector: 'common-nav',
-  templateUrl: '../view/common/common-nav.component.html'
+  templateUrl: '../view/common/common-nav.component.html',
+  directives: [ROUTER_DIRECTIVES]
 })
 export class CommonNavComponent {
   
