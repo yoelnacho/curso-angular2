6192a9d2115d5b832660e906db985d833173d576
Yo <yoelnacho@gmail.com>
2016-06-26 18:25:43 -0300

    Create datepicker component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page.component';
 import { TodoComponent } from './components/todo.component';
 import { PeliculasListComponent } from './components/peliculas-list.component';
+import { DatepickerComponent } from './components/datepicker.component';
 
 export const routes: RouterConfig = [
   {path: "", component: LandingPageComponent},
   {path: "peliculas", component: PeliculasListComponent},
-  {path: "todo", component: TodoComponent}
+  {path: "todo", component: TodoComponent},
+  {path: "datepicker", component: DatepickerComponent}
 ];
 
 export const APP_ROUTER_PROVIDERS = [
---
+++ src/app/components/datepicker.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'datepicker',
+    templateUrl: '../view/datepicker.component.html'
+})
+export class DatepickerComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
