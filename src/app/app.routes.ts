6d33b5ff94f256241c97f6659c796f3574c44aeb
Yo <yoelnacho@gmail.com>
2016-06-30 01:54:34 -0300

    component+ HTTP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page.component';
 import { TodoComponent } from './components/todo.component';
 import { PeliculasListComponent } from './components/peliculas-list.component';
+import { HTTPComponent } from './components/http.component';
 
 export const routes: RouterConfig = [
   {path: "", component: LandingPageComponent},
   {path: "peliculas", component: PeliculasListComponent},
-  {path: "todo", component: TodoComponent}
+  {path: "todo", component: TodoComponent},
+  {path: "http", component: HTTPComponent}
 ];
 
 export const APP_ROUTER_PROVIDERS = [
---
+++ src/app/components/http.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'http',
+    templateUrl: 'app/view/http.component.html'
+})
+export class HTTPComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
